cfa6399d383671f4fe618c796af82a8d1ed03228
jharna748 <77603642+jharna748@users.noreply.github.com>
2021-02-27 18:18:14 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,61 +54,7 @@ export default (() => {
 						margin="10px 0"
 						color="#652900"
 					>
-						Creating opportunities{" "}
-						<br />
-						for{" "}
-						<br />
-						pre-service{" "}
-						<br />
-						teachers
-					</Text>
-				</StackItem>
-			</Stack>
-		</Section>
-		<Section
-			padding="60px 0"
-			sm-padding="40px 0"
-			min-height="600px"
-			sm-min-height="auto"
-			background="--color-light"
-			display="flex"
-			flex-direction="column"
-		>
-			<Text
-				font="--base"
-				text-transform="uppercase"
-				letter-spacing="1px"
-				max-width="850px"
-				color="--grey"
-				margin="0px"
-			>
-				creating programs to support the holistic development of teachers in india
-			</Text>
-			<Text
-				as="h1"
-				margin="8px 0px 32px"
-				font="--headline1"
-				md-font="--headline2"
-				color="--orange"
-				max-width="850px"
-			>
-				here's how we do it
-			</Text>
-			<Stack margin-top="auto" color="--grey" font="--base">
-				<StackItem width="25%" md-width="50%" sm-width="100%">
-					<Text margin="0px">
-						We have designed a competency framework to support capacity building of pre-service teachers.{"\n\n"}
-						<br />
-					</Text>
-				</StackItem>
-				<StackItem width="25%" md-width="50%" sm-width="100%">
-					<Text margin="0px">
-						Through a network of Teaching Learning Centers (TLC) and Community Learning Center (CLC), we connect pre-service teachers to meaningful and holistic learning experiences.{"\n\n"}
-					</Text>
-				</StackItem>
-				<StackItem width="25%" md-width="50%" sm-width="100%">
-					<Text margin="0px">
-						Based on our survey with 400+ pre-service and in-service teachers, we share some insights into teacher motivations and needs, and questions worth pondering.{"\n\n"}
+						creating opportunities for pre-service teachers
 					</Text>
 				</StackItem>
 			</Stack>
@@ -124,7 +70,7 @@ export default (() => {
 				<StackItem width="75%" lg-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
-						all educators evolve to rediscover learning and reimagine education.
+						our values
 					</Text>
 					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
 						integrity
@@ -165,7 +111,7 @@ export default (() => {
 				color="--grey"
 				margin="0px"
 			>
-				evolve, rediscove, reimagine teaching...
+				our mission
 			</Text>
 			<Text
 				as="h1"
@@ -175,7 +121,7 @@ export default (() => {
 				color="--dark"
 				max-width="850px"
 			>
-				why?
+				all educators evolve to rediscover learning & reimagine education
 			</Text>
 			<Stack margin-top="auto" color="--grey" font="--base">
 				<StackItem width="25%" md-width="50%" sm-width="100%">
@@ -207,6 +153,52 @@ export default (() => {
 				<StackItem width="25%" md-width="50%" sm-width="100%">
 					<Text margin="0px">
 						Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			min-height="600px"
+			sm-min-height="auto"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Text
+				font="--base"
+				text-transform="uppercase"
+				letter-spacing="1px"
+				max-width="850px"
+				color="--grey"
+				margin="0px"
+			/>
+			<Text
+				as="h1"
+				margin="8px 0px 32px"
+				font="--headline1"
+				md-font="--headline2"
+				color="#d18f41"
+				max-width="850px"
+			>
+				creating programs to support the holistic development of teachers in india
+			</Text>
+			<Stack margin-top="auto" color="--grey" font="--base">
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						We have designed a competency framework to support capacity building of pre-service teachers.{"\n\n"}
+						<br />
+					</Text>
+				</StackItem>
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						Through a network of Teaching Learning Centers (TLC) and Community Learning Center (CLC), we connect pre-service teachers to meaningful and holistic learning experiences.{"\n\n"}
+					</Text>
+				</StackItem>
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						Based on our survey with 400+ pre-service and in-service teachers, we share some insights into teacher motivations and needs, and questions worth pondering.{"\n\n"}
 					</Text>
 				</StackItem>
 			</Stack>
